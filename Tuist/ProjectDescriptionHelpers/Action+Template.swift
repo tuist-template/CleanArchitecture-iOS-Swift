4f66699cdb014e3d6bf8d8369d8198bee81def1f
JiHoonAHN <ahnjh1028@naver.com>
2022-11-01 08:22:27 +0900

    Update Scripg

--- Tuist/ProjectDescriptionHelpers/Action+Template.swift
+++ Tuist/ProjectDescriptionHelpers/Action+Template.swift
@@ -3,6 +3,7 @@ import ProjectDescription
 public extension TargetScript {
     static let swiftLint = TargetScript.pre(
         path: Path.relativeToRoot("Scripts/SwiftLintRunScript.sh"),
-        name: "SwiftLint"
+        name: "SwiftLint",
+        basedOnDependencyAnalysis: false
     )
 }
